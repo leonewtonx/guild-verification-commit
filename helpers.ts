58184cb5e8a926b5a50451b0ae04264316a1672e
leonewtonx <leonewtonx@gmail.com>
2026-03-24 16:38:39 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T13:37:19.521Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-24T13:38:37.764Z
+Commit: Patch security vulnerability in authentication
