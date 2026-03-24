8e861bab994e735ab5c1c35f49cc412c73487f94
leonewtonx <leonewtonx@gmail.com>
2026-03-24 16:40:59 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T13:40:44.654Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-24T13:40:58.148Z
+Commit: Fix memory leak in event handlers
