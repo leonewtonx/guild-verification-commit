f914308cbc8f9953d0063e5fde39fcd684e4f44a
leonewtonx <leonewtonx@gmail.com>
2026-03-24 17:33:45 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T13:41:19.154Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-24T14:33:43.818Z
+Commit: Resolve timezone issues in date handling
