6a84b0454da930c7f9eccf78bb455598716ac3e2
leonewtonx <leonewtonx@gmail.com>
2026-03-24 16:36:48 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T13:36:08.108Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-24T13:36:46.878Z
+Commit: Fix CSS styling bug on mobile devices
