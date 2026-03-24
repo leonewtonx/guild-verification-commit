520129fbcdabb18862f30a25656f73c113c32c0f
leonewtonx <leonewtonx@gmail.com>
2026-03-24 16:40:48 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T13:39:05.253Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-24T13:40:46.825Z
+Commit: Add export functionality for reports
