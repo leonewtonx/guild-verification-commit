3b497ba1a48e119ec92942a04fdd837714611b83
leonewtonx <leonewtonx@gmail.com>
2026-03-24 16:35:37 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T13:32:05.854Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-24T13:35:35.977Z
+Commit: Fix broken links in documentation
