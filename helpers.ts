ffea4309f057f7a86032c26e0d7664528a549036
leonewtonx <leonewtonx@gmail.com>
2026-03-24 16:35:53 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T13:35:41.282Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-24T13:35:52.633Z
+Commit: Patch API endpoint error handling
