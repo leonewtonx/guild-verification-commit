ebb8871e70b084e7434e18a09d461609d06e5fc8
leonewtonx <leonewtonx@gmail.com>
2026-03-24 16:36:09 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T13:35:52.633Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-24T13:36:08.108Z
+Commit: Fix broken links in documentation
