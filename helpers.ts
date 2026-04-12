ae35dcd079f1906af0819701adf297bbfc7725bf
leonewtonx <leonewtonx@gmail.com>
2026-04-12 23:17:01 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T15:46:18.968Z
-Commit: Add export functionality for reports
+Timestamp: 2026-04-12T20:17:00.369Z
+Commit: Fix CSS styling bug on mobile devices
