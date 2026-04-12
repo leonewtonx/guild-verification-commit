9c5ff5737170f14f2bd8d41ef40cd94660fa074a
leonewtonx <leonewtonx@gmail.com>
2026-04-12 23:17:28 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-12T20:17:24.679Z
-Commit: Fix database connection timeout
+Timestamp: 2026-04-12T20:17:27.554Z
+Commit: Resolve null pointer exception in user service
