f2a9a2c0ec0e74c58e2f8dedd1deaa22ff49874a
leonewtonx <leonewtonx@gmail.com>
2026-04-12 23:16:48 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T15:46:15.030Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-04-12T20:16:47.470Z
+Commit: Fix CSS styling bug on mobile devices
