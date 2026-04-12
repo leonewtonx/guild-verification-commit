6c7a663c7a5fa22b41660137ba1a0019c41fdea0
leonewtonx <leonewtonx@gmail.com>
2026-04-12 23:17:15 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-12T20:17:00.369Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-12T20:17:14.948Z
+Commit: Fix memory leak in event handlers
