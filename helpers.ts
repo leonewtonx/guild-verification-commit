51029427ba05d087b88a2d42b3a7e23cae01d67a
leonewtonx <leonewtonx@gmail.com>
2026-04-15 04:43:11 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-15T01:42:03.421Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-15T01:43:10.981Z
+Commit: Patch API endpoint error handling
