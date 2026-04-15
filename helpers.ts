928430fa1b7efa9ad22e44e7f36547c43104bebb
leonewtonx <leonewtonx@gmail.com>
2026-04-15 04:42:04 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-15T01:41:57.641Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-04-15T01:42:03.421Z
+Commit: Fix broken links in documentation
