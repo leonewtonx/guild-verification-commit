f9d5c40f63e72b00f99f00b993807c7c183f5f44
leonewtonx <leonewtonx@gmail.com>
2026-04-15 04:42:48 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-15T01:42:20.860Z
-Commit: Implement user profile customization
+Timestamp: 2026-04-15T01:42:47.552Z
+Commit: Fix broken links in documentation
