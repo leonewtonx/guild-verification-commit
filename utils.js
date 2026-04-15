f72a2643903c065d903acede75a12e6c8275ae19
leonewtonx <leonewtonx@gmail.com>
2026-04-15 04:40:04 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-15T01:40:01.059Z
-Commit: Add export functionality for reports
+Timestamp: 2026-04-15T01:40:03.994Z
+Commit: Fix CSS styling bug on mobile devices
