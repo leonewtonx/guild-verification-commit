81ef2c367a97674514ba06d9b737ebbad5ff52f4
leonewtonx <leonewtonx@gmail.com>
2026-04-15 06:01:42 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-15T03:01:35.891Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-04-15T03:01:41.908Z
+Commit: Fix CSS styling bug on mobile devices
