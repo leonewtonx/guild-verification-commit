72a232f455d909fd2236e97b48e8eeaf6cb24f65
leonewtonx <leonewtonx@gmail.com>
2026-04-15 05:59:32 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-15T02:59:14.586Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-04-15T02:59:31.785Z
+Commit: Fix CSS styling bug on mobile devices
