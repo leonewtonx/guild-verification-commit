845c267f02677f78e535d1a74752a22ef578746e
leonewtonx <leonewtonx@gmail.com>
2026-04-15 05:58:41 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-15T02:58:24.929Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-15T02:58:41.481Z
+Commit: Fix broken links in documentation
